83bd17a7f789136847fe977c2bc2dd226317b3d1
Alin Panaitiu <alin.p32@gmail.com>
2017-12-06 21:38:20 +0200

    Add solarNoon, test non-UTC timezones and add macOS target

---
+++ Solar_macOS/Solar_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Solar_macOS.h
+//  Solar_macOS
+//
+//  Created by Alin on 06/12/2017.
+//  Copyright © 2017 Chris Howell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Solar_macOS.
+FOUNDATION_EXPORT double Solar_macOSVersionNumber;
+
+//! Project version string for Solar_macOS.
+FOUNDATION_EXPORT const unsigned char Solar_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Solar_macOS/PublicHeader.h>
+
+
