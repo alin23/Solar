6dbef0d85c1b2732ccf3339824ddb9e11a572431
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-04-10 20:35:52 +0300

    Swift Package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Solar",
+    platforms: [
+        .macOS(.v10_12)
+        .watchOS(.v3_2)
+        .iOS(.v8)
+        .tvOS(.v11_1)
+    ],
+    products: [
+        .library(name: "Solar", targets: ["Solar"]),
+    ],
+    targets: [
+        .target(name: "Solar"),
+        .testTarget(name: "Solar_Tests", dependencies: ["Solar"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
